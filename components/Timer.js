00e598788ac63555c4393c2f0de92c35b8621011
LuahJunYang <e0725519@u.nus.edu>
2022-06-15 19:47:19 +0800

    added Timer.js

---
+++ components/Timer.js
@@ -0,0 +1,13 @@
+import 'react-native-gestuer-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function Timer() {
+    return {
+        <NavigationContainer>
+        </NavigationContainer>
+    };
+}
