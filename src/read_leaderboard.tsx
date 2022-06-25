5a1ef95c7599cd1bf4ac7ad2c55b6d2744a17b6c
Zuo Hui <chenzuohui11@gmail.com>
2022-06-25 15:37:44 +0800

    added read leaderboard function

--- src/read_leaderboard.tsx
+++ src/read_leaderboard.tsx
@@ -1,35 +1,25 @@
 import { supabase } from '../lib/supabase'
 import { useState, useEffect } from "react";
-import { ApiError, Session } from "@supabase/supabase-js";
 
-export default async function readLeaderboard( { session }: { session: Session }) {
+
+export default async function readLeaderboard() {
     const user = supabase.auth.user();
+    const [results, setResults] = useState([]);
+  
+    async function getResult() {
+      //setLoading(true);
+      const user = supabase.auth.user();
+      const { data, error, status } = await supabase
+        .from("points")
+        .select(`username, pts`)
+        .eq("user_id", user.id)
+        .order('pts', {ascending: true});
+  
+      const newData = Array.from(data);
+      setResults(newData);
+    }
     useEffect(() => {
-      if (session) getProfile();
-    }, [session]);
-  
-    async function getProfile() {
-        const user = supabase.auth.user();
-        if (!user) throw new Error("No user on the session!");
-  
-        let { data, error, status } = await supabase
-          .from("profiles")
-          .select(`username, fullname`)
-          .eq("id", user.id)
-          .single();
-      }
-
-    let { data : res, error} = await supabase
-      .from('points')
-      .select('username, pts')
-      .order('pts', {ascending: true})
-  
-    if (error) {
-      console.error(error)
-      return
-    }
-    else { 
-      return res
-    }
+    getResult();
+  }, []);
 
   }
