ab7d174ec32000dddf466325b43b7ed35c1dbe89
LuahJunYang <e0725519@u.nus.edu>
2022-06-25 13:12:08 +0800

    Added statistics data template

---
+++ data/statisticsdata.js
@@ -0,0 +1,25 @@
+const statisticsData= [
+    {
+        id: 1,
+        image: require('../images/bubble-tea1.png'),
+        title: 'Single Order',
+        duration: '60 Minutes',
+        date: '12/06/2022'
+    },
+    {
+        id: 2,
+        image: require('../images/bubble-tea2.png'),
+        title: 'Group Order',
+        points: '120 Minutes',
+        date: '10/06/2022'
+    },
+    {
+        id: 3,
+        image: require('../images/bubble-tea3.png'),
+        title: 'Single Order',
+        points: '120 Minutes',
+        date: '09/06/2022'
+    },
+];
+
+export default statisticsData;
